ec8d8c3c90a144c1e47964df888b9ec8f001f41d
victor-kiss <victorkiss10@gmail.com>
2022-09-17 22:17:25 -0300

    new changes

--- pages/search-event.js
+++ pages/search-event.js
@@ -67,7 +67,9 @@ export default function SearchEvent() {
                 </Div>
               </GridDiv>
                 <FlexDiv direction="column" justify="center" align="center" padding="20px 0" bgColor="#141414" margin="20px 0">
-                  <input placeholder="event name ex: age of x" type="text" className='input-search'/>
+                 <div>
+                    <input placeholder="event name ex: age of x" type="text" className='input-search'/>
+                 </div>
                   {apiData.map((data)=> data.data.count == 0 ? <ErrorCard key={data.code}/> : <Card data={data} key={data.code}/>)}
                 </FlexDiv>
         <Footer/>
--- pages/search-series.js
+++ pages/search-series.js
@@ -97,7 +97,9 @@ export default function SearchSeries() {
               </GridDiv>
          
                 <FlexDiv direction="column" justify="center" align="center" padding="20px 0" bgColor="#141414" margin="20px 0">
-                  <input placeholder="event name ex: Age of X: Universe" type="text" className='input-search'/>
+                 <div>
+                    <input placeholder="event name ex: Age of X: Universe" type="text" className='input-search'/>
+                 </div>
                 {apiData.map((data)=> data.data.count == 0 ? <ErrorCard key={data.code}/> : 
                     <Card key={data.data.count}>
                       {
